44eadca297c1aec667e7502bbb4d6906494ff80e
Jeff <tonny0531@hotmail.com>
2021-04-24 00:08:46 +0800

    Init: Not Work

---
+++ ClassLibary/FriendClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FriendAssembly
+{
+    internal class FriendlyClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public FriendlyClass(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using FriendAssembly;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FriendlyClass friendly = new FriendlyClass(1, "MyFriend");
+            Console.WriteLine(friendly.Id + "  " + friendly.Name);
+        }
+    }
+}
